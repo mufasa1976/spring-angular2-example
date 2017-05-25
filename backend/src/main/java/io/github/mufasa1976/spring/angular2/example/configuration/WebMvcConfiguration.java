b9daf11fb26f0b7b517626f26ec317ed2f977519
mufasa1976 <mufasa1976@coolstuff.software>
2017-05-25 22:33:20 +0200

    changed the redirect to use redirect:/index.html

--- backend/src/main/java/io/github/mufasa1976/spring/angular2/example/configuration/WebMvcConfiguration.java
+++ backend/src/main/java/io/github/mufasa1976/spring/angular2/example/configuration/WebMvcConfiguration.java
@@ -1,15 +1,16 @@
 package io.github.mufasa1976.spring.angular2.example.configuration;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @Slf4j
-public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
   private final static String FRONTEND_APPLICATION = "spring-angular2-example-frontend";
 
@@ -19,16 +20,20 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.setOrder(2);
-    String frontendApplicationResource = String.format("classpath:/META-INF/resources/webjars/%s/%s/", FRONTEND_APPLICATION, version);
+    String frontendApplicationResource = prefixWithFrontendClasspath("");
     log.info("Frontend-Application: {}", frontendApplicationResource);
     registry.addResourceHandler("/**").addResourceLocations(frontendApplicationResource);
     super.addResourceHandlers(registry);
   }
 
+  private String prefixWithFrontendClasspath(String file) {
+    return String.format("classpath:/META-INF/resources/webjars/%s/%s/%s", FRONTEND_APPLICATION, version, file);
+  }
+
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.setOrder(1);
-    registry.addRedirectViewController("/", "/index.html");
+    registry.addViewController("/").setViewName("redirect:/index.html");
     registry.addViewController("/index.html").setViewName("index");
     super.addViewControllers(registry);
   }
