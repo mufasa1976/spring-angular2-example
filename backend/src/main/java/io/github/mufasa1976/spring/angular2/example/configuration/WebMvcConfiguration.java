5ff524a869ffe7d5bf1df7fd2762bb3b197040c7
mufasa1976 <mufasa1976@coolstuff.software>
2017-05-29 16:14:55 +0200

    WebMVC only serves the Angular App and the API

--- backend/src/main/java/io/github/mufasa1976/spring/angular2/example/configuration/WebMvcConfiguration.java
+++ backend/src/main/java/io/github/mufasa1976/spring/angular2/example/configuration/WebMvcConfiguration.java
@@ -2,50 +2,48 @@ package io.github.mufasa1976.spring.angular2.example.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
+@EnableWebMvc
 @Slf4j
 class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
   private final static String FRONTEND_APPLICATION = "spring-angular2-example-frontend";
+
+  private final static Map<String, String> RESOURCE_MAPPINGS;
+
+  static {
+    RESOURCE_MAPPINGS = new HashMap<>();
+    RESOURCE_MAPPINGS.put("/*.bundle.js", "/");
+    RESOURCE_MAPPINGS.put("/*.bundle.js.map", "/");
+    RESOURCE_MAPPINGS.put("/assets/**", "/assets/");
+  }
 
   @Value("${info.build.version}")
   private String version;
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.setOrder(2);
-    String frontendApplicationResource = prefixWithFrontendClasspath("");
-    log.info("Frontend-Application: {}", frontendApplicationResource);
-    registry.addResourceHandler("/**").addResourceLocations(frontendApplicationResource);
-    super.addResourceHandlers(registry);
+    registry.setOrder(0);
+    RESOURCE_MAPPINGS.forEach((path, suffix) -> registry.addResourceHandler(path).addResourceLocations(prefixWithFrontendClasspath(suffix)));
   }
 
-  private String prefixWithFrontendClasspath(String file) {
-    return String.format("classpath:/META-INF/resources/webjars/%s/%s/%s", FRONTEND_APPLICATION, version, file);
+  private String prefixWithFrontendClasspath(String suffix) {
+    return String.format("classpath:/META-INF/resources/webjars/%s/%s%s", FRONTEND_APPLICATION, version, suffix);
   }
 
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
-    registry.setOrder(1);
-    addAngularRoutes(registry);
-    super.addViewControllers(registry);
-  }
-
-  private void addAngularRoutes(ViewControllerRegistry registry) {
-    addAngularRoute("/", registry);
-    addAngularRoute("/heroes", registry);
-    addAngularRoute("/heroes/**", registry);
-    addAngularRoute("/dashboard", registry);
-  }
-
-  private void addAngularRoute(String route, ViewControllerRegistry registry) {
-    registry.addViewController(route).setViewName("app");
+    registry.addViewController("/**").setViewName("app");
   }
 
 }
